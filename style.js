0a6b6bc03001a86328d0bad1f0a9680434698b99
RobertMu94 <robertmunoz@misena.edu.co>
2023-05-31 00:15:07 -0500

    ejercicios

---
+++ style.js
@@ -0,0 +1,45 @@
+/**Ejercicio 1 */
+
+/**let respuesta = prompt("Eres bellisimo/a?");
+
+if(respuesta === "si" ){
+    console.log("Ciertamente.");    
+}else if(respuesta === "no" ){
+    console.log("No te creo.");
+}else{
+    console.log("Responda si o no.");
+}
+*/ 
+
+/**Ejercicio 2 */
+
+/**let numero = prompt("Ingrese numero");
+
+if(numero % 2 === 0 ){
+    document.write("es divisible por 2");
+
+}else{
+    document.write("no es divisible por 2");
+}
+*/
+
+/**Ejercicio 3 */
+
+/**let numero1 = prompt("Ingrese numero");
+
+if(numero1 % 2 === 0 ){
+    alert("es un numero par");
+
+}else{
+    alert("es un numero impar");
+}
+*/
+
+var numerocliente = prompt("Ingrese numero de cliente");
+
+if(numerocliente === 1000 ){
+    console.log("ganaste premio");
+
+}else if{
+    console.log( "lo sentimos sigue intentado");
+}
